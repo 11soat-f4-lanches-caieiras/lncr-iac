27b71e904cf28c6cb2f87d1e04b1b518ac3bb30f
Gustavo Silva <113210648+gustavo-log@users.noreply.github.com>
2025-11-08 11:40:58 -0300

    add: module secrets provider workflow

--- modules/alb-controller/main.tf
+++ modules/alb-controller/main.tf
@@ -62,29 +62,34 @@ resource "helm_release" "aws_load_balancer_controller" {
   namespace  = "kube-system"
   timeout    = 600
 
-  set = [
-    {
-      name  = "clusterName"
-      value = var.cluster_name
-    },
-    {
-      name  = "region"
-      value = var.region
-    },
-    {
-      name  = "serviceAccount.create"
-      value = "false"
-    },
-    {
-      name  = "serviceAccount.name"
-      value = "aws-load-balancer-controller"
-    },
-    {
-      name  = "vpcId"
-      value = var.vpc_id
-    }
-  ]
-  
+  set {
+    name  = "clusterName"
+    value = var.cluster_name
+  }
+
+  set {
+    name  = "region"
+    value = var.region
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+
+  set {
+    name  = "serviceAccount.annotations.eks.amazonaws.com/role-arn"
+    value = aws_iam_role.load_balancer_controller_role.arn
+  }
+  set {
+    name  = "vpcId"
+    value = var.vpc_id
+  }
   values = [
     <<-EOT
     tolerations:
